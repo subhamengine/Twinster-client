a7ce143cab007f459ff22e7189aea53069e4bbf8
subham sharma <subhamengine@gmail.com>
2023-04-19 01:13:40 +0530

    redux-integrated

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
+import { useState } from "react";
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
-import './App.css';
+
 
 function App() {
   return (
     <div className="app">
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />} />
+          <Route path="/home" element={<HomePage />} />
+          <Route path="/profile/:userId" element={<ProfilePage />} />
+
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
